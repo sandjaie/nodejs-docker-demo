aaf8e242b1e1b77211ade6ce07f9bb465085d9de
Sandjaie Ravi <sandjaier@gogoair.com>
2019-05-09 13:38:34 +0530

    Added: job-dsl file-nodejs.groovy

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,19 @@
+job('NodeJS') {
+    scm {
+        git('git://github.com/sandjaie/nodejs-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('sandjaie')
+            node / gitConfigEmail('sandjaie@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+
