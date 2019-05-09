ede08e1e1fe8faac4e4a7f32f5ee0e98c0b55cd6
sandjaie <sandjaie@gmail.com>
2019-05-10 01:17:42 +0530

    Edit: dsl file for pipeline

--- job-dsl/nodejs_docker_pipeline.groovy
+++ job-dsl/nodejs_docker_pipeline.groovy
@@ -1,23 +1,25 @@
-def jobName = 'nodejs-docker-pipeline'
+def jobName = 'nodejs_docker_pipeline'
 def repo = 'https://github.com/sandjaie/nodejs-docker-demo.git'
 
 pipelineJob("${jobName}") {
-    // triggers {
-    //     scm('H/5 * * * *')
-    // }
-    // description("Pipeline for $repo")
-    cpsScm {
-        scm {
-            git {
-                remote {
-                    url("${repo}")
-                }
-                branches('master')
-                extensions { 
-                    //required as otherwise it may try to tag the repo, which you may not want
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url("${repo}")
+                    }
+                    branches('master')
+                    extensions { 
+                        //required as otherwise it may try to tag the repo, which you may not want
+                    }
                 }
             }
-        }
         scriptPath('Jenkinsfile')
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    description("Pipeline for ${repo}")
     }
 }
