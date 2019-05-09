8f0ba7855d1f38785007d77279af1922db6d1a38
sandjaie <sandjaie@gmail.com>
2019-05-10 00:36:48 +0530

    Add: dsl file for pipeline

---
+++ job-dsl/nodejs_docker_pipeline.groovy
@@ -0,0 +1,23 @@
+def jobName = 'nodejs-docker-pipeline'
+def repo = 'https://github.com/sandjaie/nodejs-docker-demo.git'
+
+pipelineJob("${jobName}") {
+    triggers {
+        scm('H/5 * * * *')
+    }
+    
+    description("Pipeline for $repo")
+
+    cpsScm {
+        scm {
+            git {
+                remote { url(repo) }
+                branches('master')
+                scriptPath('jenkins-pipeline/Jenkinsfile')
+                extensions { 
+                    //required as otherwise it may try to tag the repo, which you may not want
+                }
+            }
+        }
+    }
+}
